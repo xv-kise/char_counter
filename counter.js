c293f0da806bb908c9c62eb890134f7a8685c1bc
xv-kise <salmonsushitaro@gmail.com>
2026-02-07 11:54:58 +0900

    Update counter.js
    大型アプデ

--- counter.js
+++ counter.js
@@ -1,48 +1,109 @@
-var box1 = document.getElementById("input");
+let box1 = document.getElementById("input");
+let resultdiv = document.getElementById("result");
+let regex_check = document.getElementById("regex?");
+let regexbox = document.getElementById("regexbox");
 
 //ラベル書き換え（汎用）
 function changelabel(label, next_text){ 
-    var text = document.getElementById(label);
+    let text = document.getElementById(label);
     text.innerHTML = next_text;
 }
 function count_chars(text){
-    var counts = {};
-    var i = "";
-    var convert_list = {"\n": "(改行)", " ": "(空白)", "　": "(全角空白)", "\t": "(Tab)"}
+    let counts = {};
+    let tokens =[]
+    let i = "";
+    let convert_list = {"\n": "(改行)", " ": "(空白)", "　": "(全角空白)", "\t": "(Tab)"};
     //↑単体で分かりにくい文字の置き換え対応一覧
+    let mode = "char";
+    if (regex_check.checked){
+        mode = "regex";
+    }
     //出現回数カウント
-    for (var n = 0; n < text.length; n++){
-        i = text[n];
-        if (i in convert_list){
+    if (mode == "char"){
+        for (let n = 0; n < text.length; n++){
+            //i = text[n];
+            tokens.push(text[n]);
+            /*
+            if (i in convert_list) {
+            //if (!(i in convert_list)) {
+                i = convert_list[i];}
+            
+            if (i in counts){
+                counts[i] += 1;
+            } else {
+                counts[i] = 1;
+            }//}
+            */
+        }
+    } else if (mode == "regex"){
+        //正規表現の処理
+        let pattern = RegExp(regexbox.value, "g");
+        tokens = box1.value.match(pattern)
+        if (tokens == null){
+            tokens = ["(マッチなし)"]
+        }
+    }
+
+    for (i of tokens){
+        if (i in convert_list) {
             i = convert_list[i];}
         if (i in counts){
             counts[i] += 1;
         } else {
             counts[i] = 1;
-        }
+        }//}
     }
-    var result = []
-    for (var i of Object.keys(counts)){
+
+    let result = []
+    for (let i of Object.keys(counts)){
         result.push([i, counts[i]]);
     }
     //多い順に並べ替えて表示を成型
-    result.sort((a, b) => b[1] - a[1]);
-    var temp1 = [];
-    var rank = 1;
-    var i1_before = 0;
-    for (var n = 0; n < result.length; n++){
+    result.sort((a, b) => b[1] - a[1]);//降順に並べ替える
+    let temp1 = [];
+    let rank = 1;
+    let i1_before = 0;
+    for (let n = 0; n < result.length; n++){
         i = result[n];
         //同率を同じ順位に
         if (i[1] != i1_before){
             rank = n + 1;
         }
         i1_before = result[n][1];
-        temp1.push(rank + "/<b>" + i[0] + "</b>/" + i[1] + "回");
+        temp1.push([rank, i[0] , i[1]]);
     }
-    return "順位/文字/回数<br>" + temp1.join("<br>");
+    return temp1;
 }
 //メインのラベル書き換え
+
 function count_and_display(){
     box1 = document.getElementById("input");
-    changelabel("result", count_chars(box1.value));
+    resultdiv = document.getElementById("result");
+    resultp = document.getElementById("resulttable");
+
+    //置き換え後の表作成
+    let resultlist = count_chars(box1.value);
+    let resulttable = document.createElement("table");
+    let temp2 = ""
+    let temp3 = ""
+    temp2 = document.createElement("tr");
+    for (let n of ["#", "文字", "回数"]){
+        temp3 = document.createElement("th");
+        temp3.textContent = n
+        temp2.appendChild(temp3)
+    resulttable.appendChild(temp2);
+    }
+    for (let n of resultlist){
+        temp2 = document.createElement("tr");
+        for (let m of n){
+            temp3 = document.createElement("td");
+            temp3.textContent = m;
+            temp2.appendChild(temp3);
+        }
+        resulttable.appendChild(temp2);
+    }
+    resulttable.id="resulttable";
+
+    //changelabel("result", count_chars(box1.value));
+    resultdiv.replaceChild(resulttable, resultp);
 }
